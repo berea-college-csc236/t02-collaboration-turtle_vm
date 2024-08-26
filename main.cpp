a44db6e83ca938e2ea2c51bfacadadc03f62e103
vazquezsotoj <vazquezsotoj@berea.edu>
2024-08-26 10:12:29 -0400

    Added my name, added a hat, and a flag

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
     Edits made by:
-    TODO: <Add name of student 1 Here> -- to be added by student 1
+    Javier Vazquez
     TODO: <Add name of student 2 Here> -- to be added by student 2
-    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+    
  */
 #include <iostream>
 using namespace std;
@@ -22,8 +22,9 @@ int main() {
     cin >> ducknum;
 
     for (int i = 0; i < ducknum; i++) { 
-        cout << "                __     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
-        cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
+        cout << "                     |\\  " << endl;
+        cout << "                /\\   |/ " << endl; 
+        cout << "              <(o )__|__-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
         cout << "               ( .__> /" << endl; // student 3(2) may change this line
         cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
     }
